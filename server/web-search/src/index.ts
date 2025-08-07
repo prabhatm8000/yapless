bed40699f41dac0012159620374c2bf990f11e4f
Prabhat Mishra <workforprabhat1254@gmail.com>
2025-08-07 22:11:49 +0530

    removed build cmd

--- server/web-search/src/index.ts
+++ server/web-search/src/index.ts
@@ -2,7 +2,6 @@ import express, { Express } from "express";
 import webSearchController from "./controllers/webSearchController";
 import envvars from "./lib/envvars";
 import { apiErrorHandler } from "./lib/error/apiError";
-import logger from "./lib/logger";
 
 const app: Express = express();
 const PORT = parseInt(envvars.PORT as string);
@@ -24,6 +23,5 @@ app.post("/scrape", webSearchController.scrape);
 app.use(apiErrorHandler);
 
 app.listen(PORT, () => {
-    logger(`> 'Web search' service running on port ${PORT}`);
     console.log(`\n> 'Web search' service running on port ${PORT}\n`);
 });
