88c039494b9478b729220a6355e2bccf53cbb0f5
Prabhat Mishra <workforprabhat1254@gmail.com>
2025-08-07 22:30:30 +0530

    server url added

--- client/src/constants/configs.ts
+++ client/src/constants/configs.ts
@@ -1,7 +1,7 @@
 const configs = {
     mode: import.meta.env.VITE_MODE || "dev",
     devBaseUrl: "http://localhost:8000/api/v1",
-    serverBaseUrl: "/api/v1",
+    serverBaseUrl: "https://yapless-1.onrender.com/api/v1",
     googleClientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
 };
 
